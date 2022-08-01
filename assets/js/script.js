4ad030c101ceace36452458fe4baac90012c73a6
daffa-abdllh <daffabdullah111@gmail.com>
2022-08-01 18:26:59 +0700

    baru

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbx8XjY9nXLn8yUK652wBIvckZKtB_gZcKymud8TajdSke6SV_a24mb3yxvinRUISD4qbg/exec'
-const form = document.forms['wpu-contact-form']
-const btnKirim = document.querySelector('.btn-kirim')
-const btnLoading = document.querySelector('.btn-loading')
-const alert = document.querySelector('.alert')
+const form = document.forms['contact-form']
+const send = document.querySelector('.send')
+const loading = document.querySelector('.loading')
+const sentMessage = document.querySelector('.sent-message')
       
 form.addEventListener('submit', e => {
     e.preventDefault()
     // ketika tombol kirim diklik
     // tampilkan tombol loading, hilangkan tombol kirim
-    btnLoading.classList.toggle('d-none')
-    btnKirim.classList.toggle('d-none')
+    loading.classList.toggle('d-none')
+    send.classList.toggle('d-none')
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => {
         // tampilkan tombol kirim, hilangkan tombol loading
-            btnLoading.classList.toggle('d-none')
-            btnKirim.classList.toggle('d-none')
+            loading.classList.toggle('d-none')
+            send.classList.toggle('d-none')
             // tampilkan alert
-            alert.classList.toggle('d-none')
+            sentMessage.classList.toggle('d-none')
             // reset form
             form.reset()
             console.log('Success!', response)
