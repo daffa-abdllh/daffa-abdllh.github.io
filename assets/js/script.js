bbbb69474f84a001dd4a0cf1653f825604b914eb
Daffa <daffabdullah111@gmail.com>
2022-12-24 12:36:09 +0700

    google spreadsheet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,20 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbx8XjY9nXLn8yUK652wBIvckZKtB_gZcKymud8TajdSke6SV_a24mb3yxvinRUISD4qbg/exec'
-const form = document.forms['contact-form']
-const send = document.querySelector('.send')
-const loading = document.querySelector('.loading')
-const sentMessage = document.querySelector('.sent-message')
-      
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyVQywnUfkJi0tOD9oe_8sGYcFQ1ntOxSVrAeGLGu2-amJoF-SKwr2RWUQcSWDUuWhJtw/exec';
+const form = document.forms['my-portfolio-form'];
+const send = document.querySelector('.send');
+const loading = document.querySelector('.loading');
+const alert = document.querySelector('.alert');
+
 form.addEventListener('submit', e => {
-    e.preventDefault()
-    // ketika tombol kirim diklik
-    // tampilkan tombol loading, hilangkan tombol kirim
-    loading.classList.toggle('d-none')
-    send.classList.toggle('d-none')
+    e.preventDefault();
+    send.classList.toggle('d-none');
+    loading.classList.toggle('d-none');
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => {
-        // tampilkan tombol kirim, hilangkan tombol loading
-            loading.classList.toggle('d-none')
-            send.classList.toggle('d-none')
-            // tampilkan alert
-            sentMessage.classList.toggle('d-none')
-            // reset form
-            form.reset()
-            console.log('Success!', response)
-        })
-        .catch(error => console.error('Error!', error.message))
+        send.classList.toggle('d-none');
+        loading.classList.toggle('d-none');
+        alert.classList.toggle('d-none');
+        form.reset();
+        console.log('Success!', response);
+    })
+    .catch(error => console.error('Error!', error.message))
 })
